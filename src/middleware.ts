f41335e628496c7d91f339d01e30bebf64f7bdd0
AFK <akos.kalamar@gmail.com>
2025-05-11 21:15:18 +0200

    middleware change

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,12 @@
-import createMiddleware from "next-intl/middleware";
-import { routing } from "./i18n/routing";
+import createMiddleware from 'next-intl/middleware';
+import { routing } from './i18n/routing';
 
 export default createMiddleware(routing);
 
 export const config = {
   matcher: [
-    "/((?!admin|api|test|trpc|_next|_vercel|.*\\..*).*)",
-    "/",
-    "/(en|se|hu)/:path*",
+    '/((?!admin|api|test|trpc|_next|_vercel|images/AFK-default-SEO-image.webp|.*\\..*).*)',
+    '/',
+    '/(en|se|hu)/:path*',
   ],
 };
