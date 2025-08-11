d0b8512e7b55924707ffb9ddf87a47ffaccbd9fb
AFK <akos.kalamar@kifli.tech>
2025-08-11 15:16:13 +0200

    feat: add new colors to dragonball components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 export default function Home() {
   return (
-    <div className='bg-main text-color-primary'>
-      <h1 className='text-color-primary text-4xl font-bold'>
-        Welcome to my portfolio!
-      </h1>
-      <p className='text-color-secondary'>Welcome to my portfolio!</p>
+    <div className=''>
+      <div className='flex flex-row sm:flex-col md:flex-row lg:flex-col '>
+        <h1 className='text-color-primary text-4xl font-bold'>
+          Welcome to my portfolio!
+        </h1>
+        <p className='text-color-secondary'>Welcome to my portfolio!</p>
+      </div>
+      <div className='flex flex-row sm:flex-col md:flex-row lg:flex-col p-4 bg-dragon-ball border border-dragon-ball-border rounded-lg'>
+        <h1 className='text-dragon-ball-primary text-4xl font-bold'>
+          Welcome to dragon ball!
+        </h1>
+        <p className='text-dragon-ball-secondary'>Welcome to dragon ball!</p>
+      </div>
     </div>
   );
 }
